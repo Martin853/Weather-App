2229a0ea51b6fb7ac6fb88655e6154e695576694
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-05-01 14:32:37 +0200

    Set up the weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Location } from './components/Location';
 import { Navbar } from './components/Navbar';
 
 export const App = () => {
+  const [weatherData, setWeatherData] = useState();
+  const [location, setLocation] = useState('London');
+
+  useEffect(() => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=b04816e7eca543749c6121831231004&q=${location}&aqi=no`
+    )
+      .then((response) => response.json())
+      .then((data) => setWeatherData(data));
+  }, [location]);
+
+  console.log(weatherData);
+
   return (
     <div>
       <Navbar />
