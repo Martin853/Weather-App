844b403c368627212f4c9b88f0a803ccd83f0084
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-05-01 15:59:56 +0200

    Added the unsplash api for the background image

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { Search } from './components/Search';
 export const App = () => {
   const [weatherData, setWeatherData] = useState();
   const [location, setLocation] = useState('London');
+  const [backgroundImage, setBackgroundImage] = useState(
+    'https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?crop=entropy&cs=srgb&fm=jpg&ixid=Mnw0NDMwMjJ8MHwxfHNlYXJjaHwxfHxsb25kb258ZW58MHx8fHwxNjgyOTQ1ODMw&ixlib=rb-4.0.3&q=85'
+  );
 
   useEffect(() => {
     fetch(
@@ -14,12 +17,23 @@ export const App = () => {
       .then((data) => setWeatherData(data));
   }, [location]);
 
+  useEffect(() => {
+    fetch(
+      'https://api.unsplash.com/search/photos?query=london&per_page=1&client_id=4NfpeS9-piRjRhTcsho_yLHjx1KU-s9juwwMsaP9t6A'
+    )
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+  }, [location]);
+
   console.log(weatherData);
 
   return (
     <div className="flex flex-col">
       <Navbar />
-      <div className="w-full h-full">
+      <div
+        className="w-full h-full bg-blend-soft-light bg-opacity-40 bg-black bg-no-repeat bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
         <Search />
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,9 +6,9 @@ export const Search = () => {
       <input
         type="text"
         placeholder="Search for a city"
-        className="font-openSans outline-none border-2 p-1"
+        className="font-openSans outline-none border-2 p-1 rounded-xl"
       />
-      <button className="font-openSans outline-none p-2 rounded-2xl border-2 text-lg font-semibold border-green-800 hover:bg-green-800 hover:text-white transition-all duration-300 ease-linear cursor-pointer">
+      <button className="font-openSans outline-none p-1 rounded-2xl border-2 text-lg font-semibold border-green-800 hover:bg-green-800 text-white transition-all duration-300 ease-linear cursor-pointer">
         Display in °C
       </button>
     </div>
